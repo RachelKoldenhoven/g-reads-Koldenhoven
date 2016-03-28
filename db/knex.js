fd0153a34d3f1b00d5769e4317ed2b15c64f8865
RachelKoldenhoven <rachel@koldenfrozen.com>
2016-03-28 15:12:05 -0600

    for item 116452927, created view for books, wrote query, display books on page

--- db/knex.js
+++ db/knex.js
@@ -1,3 +1,3 @@
-var environment = 'deployment' || 'development';
+var environment = process.env.NODE_ENV || 'development';
 var config = require('../knexfile')[environment];
 module.exports = require('knex')(config);

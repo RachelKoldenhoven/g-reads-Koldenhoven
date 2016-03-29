8c841b2c8cc73490a99292a3d0e68af13bdaeec8
RachelKoldenhoven <rachel@koldenfrozen.com>
2016-03-28 20:48:12 -0600

    for item 116452929, completed post route to add new book, validate server side that book title is unique

--- src/server/lib/queries.js
+++ src/server/lib/queries.js
@@ -10,9 +10,30 @@ module.exports = {
     return knex.table('books');
   },
 
+  getOneBook: function(id) {
+    return knex.table('books').where('id', id);
+  },
+
   getAuthors: function () {
     return knex.table('authors');
+  },
+
+  getSomeAuthors: function(id) {
+    return knex.table('authors').where('book_id', id);
+  },
+
+  addNewBooK: function(newBook) {
+    return knex.insert({
+      title: newBook.title,
+      book_genre: newBook.book_genre,
+      description: newBook.description,
+      book_cover_url: newBook.book_cover_url
+    }).table('books').returning('id');
+  },
+
+  addNewAuthor: function(newAuthor) {
+    return knex.insert( {
+
+    })
   }
-
-
 };
